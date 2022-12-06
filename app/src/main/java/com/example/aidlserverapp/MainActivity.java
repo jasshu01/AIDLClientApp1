27dd2140e0e6be60e9a7a54f1ba95323d09990f3
jasshugarg <jashugarg266@gmail.com>
2022-12-06 19:28:05 +0530

    Initial commit - Communication with AIDL Server App

---
+++ app/src/main/java/com/example/aidlserverapp/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.aidlserverapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.aidlclientapp1.R;
+
+import java.io.LineNumberReader;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    AIDLServer aidlServerService = null;
+
+    private ServiceConnection myServiceConnection = new ServiceConnection() {
+
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            aidlServerService = AIDLServer.Stub.asInterface(iBinder);
+            Log.d("myaidlclient", "Connected to Server");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            aidlServerService = null;
+            Log.d("myaidlclient", "DisConnected From Server");
+
+        }
+    };
+
+    @Override
+    @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent("com.jasshugarg.myAIDLSERVER");
+//        intent.setPackage("com.example.aidlserverapp");
+//        intent.setComponent(new ComponentName("com.example.aidlserverapp","com.example.aidlserverapp.MyAIDLServerService"));
+//          getApplicationContext().bindService(intent, myServiceConnection, BIND_AUTO_CREATE);
+        bind(intent);
+
+
+        Button button = findViewById(R.id.button);
+
+        Log.d("myaidlclient", "Client App");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    if (aidlServerService == null)
+                        bind(intent);
+                    if (aidlServerService != null)
+                        aidlServerService.setDisplayData(getPackageName(), getTaskId(), "Hello From Client");
+
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    public void bind(Intent intent) {
+
+//        intent.setClassName("com.example.aidlserverapp", "com.example.aidlserverapp.MyAIDLServerService");
+
+        intent.setComponent(new ComponentName("com.example.aidlserverapp", "com.example.aidlserverapp.MyAIDLServerService"));
+
+
+
+        if (getBaseContext().getApplicationContext().bindService(intent, myServiceConnection, getApplicationContext().BIND_AUTO_CREATE)) {
+            Log.v("myaidlclient", "Bind service Succeeded");
+        } else {
+            Log.v("myaidlclient", "Bind service failed");
+        }
+
+    }
+}
